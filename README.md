10dc0421d51100d2b364138ad103c164e237f51a
Nextross <70912486+Nextross@users.noreply.github.com>
2026-01-22 18:44:51 +0100

    Wedos Caddy module

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# wedos
+WEDOS module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with [Wedos](https://www.wedos.com/).
+
+## Caddy module name
+
+```
+dns.providers.wedos
+```
+
+## Building using `xcaddy`
+
+To build Caddy with this module, use the following command:
+
+```aiignore
+xcaddy build --with github.com/caddyserver/dnsproviders/wedos
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "wedos",
+				"username": "{env.WEDOS_USERNAME}",
+                "password": "{env.WEDOS_PASSWORD}"
+			}
+		}
+	}
+}
+```
+
+or with the Caddyfile:
+
+```
+# globally
+{
+	acme_dns wedos {
+		# username "YOUR_WEDOS_USERNAME"
+		# password "YOUR_WEDOS_PASSWORD"
+		# OR
+		username {$WEDOS_USERNAME}
+		password {$WEDOS_PASSWORD}
+	}
+}
+
+# Replace 'example.com' with a domain you own and want to manage via Wedos
+example.com {
+	respond "Hello, Caddy with Wedos!"
+}
+```
---
+++ wedos.go
@@ -0,0 +1,108 @@
+package wedos
+
+import (
+	"fmt"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/libdns/wedos"
+)
+
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct{ *wedos.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.wedos",
+		New: func() caddy.Module { return &Provider{new(wedos.Provider)} },
+	}
+}
+
+// Provision sets up the module. Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	repl := caddy.NewReplacer()
+	p.Provider.Username = repl.ReplaceAll(p.Provider.Username, "")
+	p.Provider.Password = repl.ReplaceAll(p.Provider.Password, "")
+
+	if p.Provider.Username == "" || p.Provider.Password == "" {
+		return fmt.Errorf("missing username and/or password")
+	}
+
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	wedos {
+//	    username {env.WEDOS_USERNAME}
+//	    password {env.WEDOS_PASSWORD}
+//	}
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			username := d.Val()
+			if d.NextArg() {
+				password := d.Val()
+				p.Provider.Username = username
+				p.Provider.Password = password
+			} else {
+				return d.ArgErr()
+			}
+		}
+
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "username":
+				if p.Provider.Username != "" {
+					return d.Err("Username already set")
+				}
+
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+
+				p.Provider.Username = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+
+			case "password":
+				if p.Provider.Password != "" {
+					return d.Err("Password already set")
+				}
+
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+
+				p.Provider.Password = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.Username == "" || p.Provider.Password == "" {
+		return d.Err("missing username and/or password")
+	}
+
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
